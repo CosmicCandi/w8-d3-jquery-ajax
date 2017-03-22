9b70901647a1e09628c107f660d97e516a38f89d
CosmicCandi <kalea.wolff@gmail.com>
2017-03-22 19:52:08 -0400

    Finishes explorer mode. Time formatted nicely with moment.

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  chirps = $.get({url: 'https://sleepy-gorge-91783.herokuapp.com/chirps/timeline', data_type: 'json'})
+    .done(function(response){
+      response.forEach(function(chirp){
+        $('#chirp_content').append(
+          `
+            <div class="col-sm-4">
+              <div class="media">
+                <div class="media-left media-middle">
+                  <img class="img-circle media-object" height="64" width="64" src="${chirp.user.image}" alt="${chirp.user.username}'s avatar">
+                  <span class="text-center">${chirp.user.username}</span>
+                </div>
+
+                <div class="media-body">
+                  ${chirp.body}<br />
+                  Created: <span class="created_at"> ${moment(chirp.created_at).format('MMMM Do YYYY, h:mm:ss a')}</span>
+                </div>
+              </div>
+            </div>
+          `
+        )
+      })
+   })
+})
